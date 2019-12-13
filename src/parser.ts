5681625a1d4429915d02e719b9e045e1d6a3cbf4
Adrien de Peretti <adrien.deperetti@gmail.com>
2019-12-13 09:30:12 +0100

    Update reg to be able to match a word with accent

--- src/parser.ts
+++ src/parser.ts
@@ -5,11 +5,11 @@ export type TextTree = TextNode | string;
  * Parse the input string to produce a tree of the nested tag
  */
 function stringToTree(text: string) {
-  const tokens = text.split(/(<{\w+\||}>)/);
+  const tokens = text.split(/(<{[A-zÀ-ū]+\||}>)/);
   const stringToTree = (tokens: string[]): [TextNode, string[]] => {
     if (tokens.length === 0) return [["", []], []];
     const [head, ...tail] = tokens;
-    const match = head.match(/<{(\w+)\|/);
+    const match = head.match(/<{([A-zÀ-ū]+)\|/);
     const styleName = match && match[1];
     if (styleName) {
       const [[_, children], tokensLeft] = stringToTree(tail);
